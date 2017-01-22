ba02f5e49da3de3d3f2a4d2927b19ee0f7b9ecb1
dylanhart <dylan96hart@gmail.com>
2017-01-21 21:24:04 -0800

    added ajax call for results

--- fakenews/urls.py
+++ fakenews/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'fn'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^detect$', views.detect, name='detect'),
+    url(r'^result/(?P<url_hash>\w+)$', views.results, name='results'),
 ]
--- fakenews/views.py
+++ fakenews/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, reverse
+
+import json
+from hashlib import md5
 
 # Create your views here.
 
 def index(req):
     return render(req, 'fakenews/index.html')
+
+@require_http_methods(['POST'])
+def detect(req):
+    hasher = md5()
+    url = req.POST.get('url', None)
+    if not url:
+        res = {'success': False, 'msg': 'url is required'}
+        return HttpResponse(json.dumps(res), content_type='application/json')
+
+    hasher.update(url.encode('utf-8'))
+    url_hash = hasher.hexdigest()
+    res = {
+        'success': True,
+        'results': reverse('fn:results', kwargs={'url_hash': url_hash}),
+    }
+
+    return HttpResponse(json.dumps(res), content_type='application/json')
+
+def results(req, url_hash):
+    return HttpResponse('not implemented')
