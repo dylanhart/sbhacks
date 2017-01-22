83d87a51cf7a84205827b82352edb10db12c50ba
dylanhart <dylan96hart@gmail.com>
2017-01-21 22:31:17 -0800

    add --no-input to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pkill gunicorn
-python3 manage.py migrate --settings sbhacks.server_settings
-python3 manage.py collectstatic --settings sbhacks.server_settings
+python3 manage.py migrate --settings sbhacks.server_settings --no-input
+python3 manage.py collectstatic --settings sbhacks.server_settings --no-input
 gunicorn sbhacks.wsgi -D
