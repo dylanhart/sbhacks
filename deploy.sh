8bba4179a1458780331b68c3f668fe22cf8c4ed4
dylanhart <dylan96hart@gmail.com>
2017-01-21 20:23:24 -0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pkill gunicorn
-python3 manage.py migrate --settings sbhacks.sever_settings
-python3 manage.py collectstatic --settings sbhacks.sever_settings
+python3 manage.py migrate --settings sbhacks.server_settings
+python3 manage.py collectstatic --settings sbhacks.server_settings
 gunicorn sbhacks.wsgi -D
