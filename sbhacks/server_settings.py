8f857cf97dca8ef5302986e6cd689a246023665e
dylanhart <dylan96hart@gmail.com>
2017-01-21 18:46:30 -0800

    add domain

--- sbhacks/server_settings.py
+++ sbhacks/server_settings.py
@@ -3,5 +3,5 @@ import os
 from .settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'spoofproof.net']
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
